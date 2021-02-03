294df27f8366fb944430e50a82e363568f1a2d10
MrChampion <66367029+AdminChamp@users.noreply.github.com>
2021-02-03 14:33:49 +0100

    Add files via upload

--- The Windows ERROR Maker.bat
+++ The Windows ERROR Maker.bat
@@ -1,24 +1,26 @@
 @echo off
-title Windows ERROR Generator
-color f0
-set /p ti=Title for ERROR: 
-set /p te=Text for ERROR: 
+title Generatore di Errori di Windows
+color b0
+set /p ti=Titolo: 
+set /p te=Testo: 
 color e
 cls
 :buttons
-echo Title: %ti%
-echo Text: %te%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Titolo: %ti%
+echo Testo: %te%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
-echo Which buttons?
+echo Scegli i pulsanti:
 echo -------------------------
 echo 0 - OK
-echo 1 - OK, Cancel
-echo 2 - Cancel, Try again, Ignore
-echo 3 - Yes, No, Cancel
-echo 4 - Yes,No
-echo 5 - Try again, cancel
+echo 1 - OK, Annulla
+echo 2 - Annulla, Riprova, Ignora
+echo 3 - Si, No, Annulla
+echo 4 - Si, No
+echo 5 - Riprova, Annulla
 echo -------------------------
-set /p b=Please enter a number: 
+set /p b=Inserisci un numero: 
 if "%b%" == "0" goto ft
 if "%b%" == "1" goto ft
 if "%b%" == "2" goto ft
@@ -28,18 +30,20 @@ if "%b%" == "5" goto ft
 goto ne
 :ft
 cls
-color c
-echo Title: %ti%
-echo Text: %te%
+color e
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Titolo: %ti%
+echo Testo: %te%
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
-echo Which message type?
+echo Scegli il tipo di messaggio:
 echo -------------------------
-echo 16 - ERROR
-echo 32 - Question
-echo 48 - Caution
-echo 64 - Info
+echo 16 - Errore
+echo 32 - Domanda
+echo 48 - Attenzione
+echo 64 - Informazione
 echo -------------------------
-set /p f=Please enter a number: 
+set /p f=Inserisci un numero: 
 if "%f%" == "16" goto final
 if "%f%" == "32" goto final
 if "%f%" == "48" goto final
@@ -48,18 +52,18 @@ goto nee
 :final
 cls
 color c
-echo x=msgbox("%te%", %b%+%f%, "%ti%")>"%Userprofile%\Desktop\Err0r.vbs"
+echo x=msgbox("%te%", %b%+%f%, "%ti%")>"%Userprofile%\Desktop\Errore.vbs"
 exit
 R U looking in the Code?
 :ne
 cls
-echo Enter a valid number
+echo Inserisci un numero valido.
 pause
 cls
 goto buttons
 :nee
 cls
-echo Enter a valid number
+echo Inserisci un numero valido.
 pause
 cls
 goto ft
